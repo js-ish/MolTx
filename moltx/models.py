2566c93d86da38ee0ccd0ee4f399a450179a1463
Michael Ding <yandy.ding@gmail.com>
2024-04-25 16:18:23 +0800

    improve tkz

--- moltx/models.py
+++ moltx/models.py
@@ -1,7 +1,33 @@
 import typing
 import torch
 import torch.nn as nn
-from moltx import nets
+from moltx import nets, tokenizers
+
+
+class AdaMRTokenizerConfig:
+    Pretrain = tokenizers.MoltxPretrainConfig(
+        token_size=512,
+        fmt='smiles',
+        spe=True,
+        spe_dropout=0.2,
+        spe_merges=240
+    )
+
+    Generation = tokenizers.MoltxPretrainConfig(
+        token_size=512,
+        fmt='smiles',
+        spe=False,
+        spe_dropout=1.0,
+        spe_merges=240
+    )
+
+    Prediction = tokenizers.MoltxPretrainConfig(
+        token_size=512,
+        fmt='smiles',
+        spe=True,
+        spe_dropout=0.0,
+        spe_merges=240
+    )
 
 
 class AdaMR(nets.AbsPosEncoderDecoder):
