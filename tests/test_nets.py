2ade71817a87c74b39c2e9b00aa1884afad0b2a5
Timo Hu <hutianmu@ish.org.cn>
2024-04-25 07:21:43 +0000

    fix:removed duplicated test, fixed last line

--- tests/test_nets.py
+++ tests/test_nets.py
@@ -49,6 +49,17 @@ def test_AbsPosEncoderDecoder():
     gen = model.forward_generation(tokens, tokens)
     assert gen.shape == (32, 16)
 
+    conf = nets.AbsPosEncoderDecoderConfig(
+        token_size=16, max_len=32, d_model=8, nhead=2,
+        num_encoder_layers=2, num_decoder_layers=2, dtype=torch.bfloat16)
+    model = nets.AbsPosEncoderDecoder(conf=conf)
+    model.eval()
+    model.requires_grad_(False)
+    
+    batch = torch.randint(1, 16, (2, 32))
+    out = model.forward_(batch, batch)
+    assert out.shape == (2, 32, 8)
+
 
 def test_AbsPosEncoderCausal():
     conf = nets.AbsPosEncoderCausalConfig(
@@ -75,33 +86,6 @@ def test_AbsPosEncoderCausal():
     gen = model.forward_generation(tokens)
     assert gen.shape == (32, 16)
 
-def test_AbsPosEncoderDecoder_bf16():
-    conf = nets.AbsPosEncoderDecoderConfig(
-        token_size=16, max_len=32, d_model=8, nhead=2,
-        num_encoder_layers=2, num_decoder_layers=2, dtype=torch.bfloat16)
-    model = nets.AbsPosEncoderDecoder(conf=conf)
-    model.eval()
-    model.requires_grad_(False)
-
-    batch = torch.randint(1, 16, (2, 32))
-    out = model.forward_(batch, batch)
-    assert out.shape == (2, 32, 8)
-    feat = model.forward_feature(batch, batch)
-    assert feat.shape == (2, 8)
-    assert feat.eq(out[:, -1]).all()
-    gen = model.forward_generation(batch, batch)
-    assert gen.shape == (2, 32, 16)
-
-    tokens = torch.randint(1, 16, (32,))
-    out = model.forward_(tokens, tokens)
-    assert out.shape == (32, 8)
-    feat = model.forward_feature(tokens, tokens)
-    assert feat.shape == (8,)
-    assert feat.eq(out[-1]).all()
-    gen = model.forward_generation(tokens, tokens)
-    assert gen.shape == (32, 16)
-
-def test_AbsPosEncoderCausal_bf16():
     conf = nets.AbsPosEncoderCausalConfig(
         token_size=16, max_len=32, d_model=8, nhead=2, num_layers=2, dtype=torch.bfloat16)
     model = nets.AbsPosEncoderCausal(conf=conf)
@@ -111,17 +95,4 @@ def test_AbsPosEncoderCausal_bf16():
     batch = torch.randint(1, 16, (2, 32))
     out = model.forward_(batch)
     assert out.shape == (2, 32, 8)
-    feat = model.forward_feature(batch)
-    assert feat.shape == (2, 8)
-    assert feat.eq(out[:, -1]).all()
-    gen = model.forward_generation(batch)
-    assert gen.shape == (2, 32, 16)
-
-    tokens = torch.randint(1, 16, (32,))
-    out = model.forward_(tokens)
-    assert out.shape == (32, 8)
-    feat = model.forward_feature(tokens)
-    assert feat.shape == (8,)
-    assert feat.eq(out[-1]).all()
-    gen = model.forward_generation(tokens)
-    assert gen.shape == (32, 16)
+    
