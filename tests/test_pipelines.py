267203c477fcd145a5be4a7eaee9df32646bc3d0
origyZ <49225887+origyZ@users.noreply.github.com>
2024-04-26 18:02:02 +0800

    fix: remove adamr pipline input k (#27)

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -4,23 +4,13 @@ from moltx import pipelines, models
 
 def test_AdaMR(tokenizer, model_conf):
     pipeline = pipelines.AdaMR(tokenizer, models.AdaMR(model_conf))
-    out = pipeline("CC[N+](C)(C)Br", k=1)
-    assert 'smiles' in out and 'probabilities' in out
-    assert isinstance(out['smiles'], list) and len(out['smiles']) == 1 and isinstance(out['smiles'][0], str)
-    assert isinstance(out['probabilities'], list) and len(
-        out['probabilities']) == 1
+    out = pipeline("CC[N+](C)(C)Br")
+    assert 'smiles' in out and 'probability' in out
+    assert isinstance(out['smiles'], str)
 
-    out = pipeline("CC[N+](C)(C)Br", k=2, gentype='beam')
-    assert 'smiles' in out and 'probabilities' in out
-    assert isinstance(out['smiles'], list) and len(out['smiles']) == 2 and isinstance(out['smiles'][0], str)
-    assert isinstance(out['probabilities'], list) and len(
-        out['probabilities']) == 2
-
-    out = pipeline("CC[N+](C)(C)Br", k=1, gentype='beam')
-    assert 'smiles' in out and 'probabilities' in out
-    assert isinstance(out['smiles'], list) and len(out['smiles']) == 1 and isinstance(out['smiles'][0], str)
-    assert isinstance(out['probabilities'], list) and len(
-        out['probabilities']) == 1
+    out = pipeline("CC[N+](C)(C)Br", gentype='beam')
+    assert 'smiles' in out and 'probability' in out
+    assert isinstance(out['smiles'], str)
 
 
 def test_AdaMRClassifier(tokenizer, model_conf):
